ffbc685ece7a648885a49d6a7b0491c22bd78d7e
Gabbi Fisher <gabbifisher@gmail.com>
2016-11-14 22:07:46 -0800

    added memberberry sayings

--- src/index.js
+++ src/index.js
@@ -27,19 +27,18 @@ var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-u
  * Array containing space facts.
  */
 var FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "Oh, I member!",
+    "Member Chewbacca?",
+    "Member Ghostbusters?",
+    "Member Slimers?",
+    "Member the Fugitive?",
+    "Member Stormtroopers?",
+    "Member Bionic Man?",
+    "Member Millennium Falcon?",
+    "Member feeling safe?",
+    "Member no ISIS?",
+    "Member Reagan?",
+    "Member Spock?"
 ];
 
 /**
@@ -85,7 +84,7 @@ Fact.prototype.intentHandlers = {
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("You can say tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+        response.ask("Do you want to member, or, you can say exit... What can I help you member?", "What can I help you member?");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
@@ -108,8 +107,8 @@ function handleNewFactRequest(response) {
     var randomFact = FACTS[factIndex];
 
     // Create speech output
-    var speechOutput = "Here's your fact: " + randomFact;
-    var cardTitle = "Your Fact";
+    var speechOutput = "Oh " + randomFact;
+    var cardTitle = "Memberberry";
     response.tellWithCard(speechOutput, cardTitle, speechOutput);
 }
 
@@ -119,4 +118,3 @@ exports.handler = function (event, context) {
     var fact = new Fact();
     fact.execute(event, context);
 };
-
